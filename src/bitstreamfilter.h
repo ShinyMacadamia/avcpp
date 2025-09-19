2c0c19a1f2663a5a33aa3cff50c921758ff8aa64
Lighty0410 <crackon0410@gmail.com>
2025-09-19 15:33:33 +0300

    uncomment after test

--- src/bitstreamfilter.h
+++ src/bitstreamfilter.h
@@ -1,39 +1,39 @@
-// #pragma once
-//
-// #include "averror.h"
-// #include "avutils.h"
-// #include "codecparameters.h"
-// #include "ffmpeg.h"
-// #include "packet.h"
-// #include <string_view>
-// extern "C" {
-// #include <libavcodec/bsf.h>
-// }
-//
-// namespace av {
-// class BitStreamFilter : public FFWrapperPtr<AVBSFContext>, public noncopyable
-// {
-// public:
-//     BitStreamFilter() = default;
-//     BitStreamFilter(BitStreamFilter&& other);
-//     friend void swap(BitStreamFilter& lhs, BitStreamFilter& rhs) noexcept;
-//     BitStreamFilter& operator=(BitStreamFilter&& other);
-//     ~BitStreamFilter();
-//
-//     void setName(std::string_view name, OptionalErrorCode ec);
-//
-//     void setInCodecParameters(CodecParametersView codecpar, OptionalErrorCode ec);
-//     void setOutCodecParameters(CodecParametersView codecpar, OptionalErrorCode ec);
-//
-//     void init();
-//
-//     bool isInitilized() const;
-//
-//     void sendPacket(Packet& packet, OptionalErrorCode ec);
-//     void receivePacket(Packet& packet, OptionalErrorCode ec);
-//
-// private:
-//     bool m_is_initialized = false;
-// };
-//
-// } // namespace av
+#pragma once
+
+#include "averror.h"
+#include "avutils.h"
+#include "codecparameters.h"
+#include "ffmpeg.h"
+#include "packet.h"
+#include <string_view>
+extern "C" {
+#include <libavcodec/bsf.h>
+}
+
+namespace av {
+class BitStreamFilter : public FFWrapperPtr<AVBSFContext>, public noncopyable
+{
+public:
+    BitStreamFilter() = default;
+    BitStreamFilter(BitStreamFilter&& other);
+    friend void swap(BitStreamFilter& lhs, BitStreamFilter& rhs) noexcept;
+    BitStreamFilter& operator=(BitStreamFilter&& other);
+    ~BitStreamFilter();
+
+    void setName(std::string_view name, OptionalErrorCode ec);
+
+    void setInCodecParameters(CodecParametersView codecpar, OptionalErrorCode ec);
+    void setOutCodecParameters(CodecParametersView codecpar, OptionalErrorCode ec);
+
+    void init();
+
+    bool isInitilized() const;
+
+    void sendPacket(Packet& packet, OptionalErrorCode ec);
+    void receivePacket(Packet& packet, OptionalErrorCode ec);
+
+private:
+    bool m_is_initialized = false;
+};
+
+} // namespace av
